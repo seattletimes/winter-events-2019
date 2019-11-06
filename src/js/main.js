abb4118bfeb45de5ea70489de50c257dd84ffba9
Miranda Hardy <mhardy@seattletimes.com>
2019-11-06 09:58:57 -0800

    implementing search

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-// require("./lib/ads");
-// var track = require("./lib/tracking");
+require("./lib/ads");
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(11595997), 3000);
+
+var track = require("./lib/tracking");
+
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+var scrollTo = require("./lib/animateScroll");
+
+var catList = document.querySelector(".filters .categories");
+var grid = document.querySelector(".grid-container");
+var searchBox = document.querySelector(".filters .search");
+
+function search(){
+  var re = document.getElementById('search');
+  console.log(re);
+}
+
+
+searchBox.addEventListener("keyup", search);
 
 require("component-responsive-frame/child");
