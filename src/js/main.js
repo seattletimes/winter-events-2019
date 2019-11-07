378b826c1fc3aa41f7422d062747b342aaf2cffd
Miranda Hardy <mhardy@seattletimes.com>
2019-11-07 10:51:59 -0800

    Search works and Filter works with some minor bugs

--- src/js/main.js
+++ src/js/main.js
@@ -13,8 +13,10 @@ var grid = document.querySelector(".grid-container");
 var searchBox = document.querySelector(".filters .search");
 var events = document.querySelectorAll(".event");
 
+
 var show = [];
 
+//debug so that the list is 100% correct. Right now it updates well but there is a bug when you filter items out after including them
 function filterByCategory(){
   if (show.includes(this.dataset.category)){
     show.pop(this.dataset.category);
@@ -26,6 +28,7 @@ function filterByCategory(){
   events.forEach(function(element){
     if(show.includes(element.dataset.category)){
       //console.log('in list');
+      element.style.display="inline";
     }
     else {
       //console.log('not in list');
@@ -33,14 +36,31 @@ function filterByCategory(){
     }
     //console.log(element.dataset.category);
   })
-  //console.log(show);
+  console.log(show);
   //console.log(this.dataset.category);
 }
 
+function filterBySearch(){
+  var searchValue = searchBox.value.toLowerCase();
+  events.forEach(function(element){
+
+    var eventText = element.innerText.toLowerCase();
+
+
+    if (eventText.search(searchValue) == -1){
+      element.style.display="none";
+    }
+    else{
+      element.style.display="inline";
+    }
+  })
+}
 
 catList.forEach(function(element){
    element.addEventListener("click", filterByCategory);
 
 })
+
+searchBox.addEventListener("keyup", filterBySearch);
 
 
