1823b0473f81284ffe1b56f70b553bb72636419a
Miranda Hardy <mhardy@seattletimes.com>
2019-11-08 16:26:34 -0800

    Javascript very broken (progress on combine filters) styling close

--- src/js/main.js
+++ src/js/main.js
@@ -13,53 +13,80 @@ var catList = document.querySelectorAll(".filter-buttons");
 var searchBox = document.querySelector(".filters .search");
 var events = document.querySelectorAll(".event");
 var resultBox = document.querySelector(".noResults");
+var header = document.querySelector(".filters");
+var sticky = header.offsetTop;
 
 
 var hide = [];
 
+// function filterByCategory(){
+//   if (hide.includes(this.dataset.category)){
+//     indexCat = hide.indexOf(this.dataset.category);
+//     hide.splice(indexCat, 1);
+//   }
+//   else {
+//     hide.push(this.dataset.category);
+//   }
+//   console.log(hide);
+
+
+
+//   if(hide.length >= 6){
+//     noResults();
+//   }
+
+//   events.forEach(function(element){
+//     if(hide.includes(element.dataset.category)){
+//       //console.log('in list');
+//       element.style.display="none";
+//     }
+//     else {
+//       //console.log('not in list');
+//       element.style.display="inline";
+//     }
+//   })
+// }
 function filterByCategory(){
   if (hide.includes(this.dataset.category)){
-    indexCat = hide.indexOf(this.dataset.category);
-    hide.splice(indexCat, 1);
+        indexCat = hide.indexOf(this.dataset.category);
+        hide.splice(indexCat, 1);
   }
   else {
     hide.push(this.dataset.category);
   }
-
-  if(hide.length >= 6){
-    noResults();
-  }
-
-  events.forEach(function(element){
-    if(hide.includes(element.dataset.category)){
-      //console.log('in list');
-      element.style.display="none";
-    }
-    else {
-      //console.log('not in list');
-      element.style.display="inline";
-    }
-  })
+  
+  return hide;
 }
+
+
+
+// function filterBySearch(){
+//   var searchValue = searchBox.value.toLowerCase();
+//   var results = 0;
+
+//   events.forEach(function(element){
+//     var eventText = element.innerText.toLowerCase();
+//     if (eventText.search(searchValue) == -1){
+//       element.style.display="none";
+//     }
+//     else{
+//       element.style.display="inline";
+//       results =+ 1;
+//     }
+//   })
+//   console.log(results);
+//   if(results == 0){
+//     noResults();
+//   }
+// }
 
 function filterBySearch(){
   var searchValue = searchBox.value.toLowerCase();
-  var results = 0;
+  return searchValue;
+}
 
-  events.forEach(function(element){
-    var eventText = element.innerText.toLowerCase();
-    if (eventText.search(searchValue) == -1){
-      element.style.display="none";
-    }
-    else{
-      element.style.display="inline";
-      results =+ 1;
-    }
-  })
-
-  if(results == 0){
-    noResults();
-  }
+function combineFilters(hideList, searchString){
+  if(hideList)
 }
 
 // function filterCombo(){
@@ -86,13 +113,22 @@ function filterBySearch(){
 // }
 
 function noResults(){
-  resultBox.style.visibility="visible";
+  resultBox.style.display="block";
+}
+
+function fixNav(){
+  if(window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } 
+  else{
+    header.classList.remove("sticky");
+  }
 }
 
 catList.forEach(function(element){
    element.addEventListener("click", filterByCategory);
 })
-
 searchBox.addEventListener("keyup", filterBySearch);
+window.onscroll = function() {fixNav()};
 
 
